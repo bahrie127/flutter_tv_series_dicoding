0f0ee56ae11aafdde0f91fd1524f45c020cc8646
bahri <saiful.bahri.tl@gmail.com>
2023-03-19 22:59:51 +0700

    integration with firebase analytics & crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -25,14 +25,20 @@ import 'package:ditonton/presentation/pages/top_rated_tv_series_page.dart';
 import 'package:ditonton/presentation/pages/tv_series_detail_page.dart';
 import 'package:ditonton/presentation/pages/tv_series_page.dart';
 import 'package:ditonton/presentation/pages/watchlist_movies_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ditonton/injection.dart' as di;
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SSLPinning.init();
   di.init();
   runApp(MyApp());
